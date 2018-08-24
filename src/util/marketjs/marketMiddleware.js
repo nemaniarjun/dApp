614326f8e1797b7533615886ba58b0fa8af6842e
travisdmathis <travis@grillwork.io>
2018-08-24 15:16:24 -0400

    reintroduces decimals until we have a fix in marketjs

--- src/util/marketjs/marketMiddleware.js
+++ src/util/marketjs/marketMiddleware.js
@@ -58,31 +58,41 @@ const depositCollateralAsync = (amount, str = store) => {
     from: web3.eth.coinbase
   };
 
-  marketjs
-    .approveCollateralDepositAsync(
-      simExchange.contract.key,
-      new BigNumber(toBaseUnit(amount.number, 18)),
-      txParams
-    )
-    .then(res => {
-      console.log('res', res);
-      return web3.eth.getTransactionReceiptMined(res).then(function() {
-        marketjs
-          .depositCollateralAsync(
-            simExchange.contract.key,
-            new BigNumber(toBaseUnit(amount.number, 18)),
-            txParams
-          )
-          .then(res => {
-            showMessage(
-              'success',
-              'Deposit successful, your transaction will process shortly.',
-              5
-            );
-            return res;
-          });
+  let collateralTokenContractInstance = web3.eth
+    .contract(abi)
+    .at(simExchange.contract.COLLATERAL_TOKEN_ADDRESS);
+
+  collateralTokenContractInstance.decimals.call((err, decimals) => {
+    // NOTE: we are calling approve on the abi used above, this it outside of MARKET.js and therefore
+    // needs to use the market collateral pool address.  We will add functionality in MARKET.js to simplify this
+    // and no longer need to use the Collateral Pool Address.
+    marketjs
+      .approveCollateralDepositAsync(
+        simExchange.contract.key,
+        new BigNumber(toBaseUnit(amount.number, decimals)),
+        txParams
+      )
+      .then(res => {
+        console.log('res', res);
+
+        return web3.eth.getTransactionReceiptMined(res).then(function() {
+          marketjs
+            .depositCollateralAsync(
+              simExchange.contract.key,
+              new BigNumber(toBaseUnit(amount.number, decimals)),
+              txParams
+            )
+            .then(res => {
+              showMessage(
+                'success',
+                'Deposit successful, your transaction will process shortly.',
+                5
+              );
+              return res;
+            });
+        });
       });
-    });
+  });
 };
 
 const getUserUnallocatedCollateralBalanceAsync = (
